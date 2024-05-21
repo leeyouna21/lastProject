d9380e072e46e2722b2a90012fe5edcfdf170a12
leeyouna21 <yuna5845@gmail.com>
2024-05-22 00:30:32 +0900

    fitst

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Main from "./components/Main/Main";
+import Login from "./components/Login/Login";
+import Travel from "./components/Travel/Travel"
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Navigate to="/main" />} />
+          <Route path="/Main" element={<Main />} />
+          <Route path="/Login" element={<Login />} />
+          <Route path="/Travel" element={<Travel />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/components/Travel/Travel.jsx
@@ -0,0 +1,76 @@
+import React, { useState, useEffect } from 'react';
+
+const Input = (props) => {
+    return <input {...props} />;
+};
+
+const Travel = () => {
+    const images = [
+        "/image/Travel_1.png",
+        "/image/Travel_2.png",
+        "/image/Travel_3.png"
+    ];
+    const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCurrentImageIndex((prevIndex) =>
+                prevIndex === images.length - 1 ? 0 : prevIndex + 1
+            );
+        }, 15000); // 15초마다 이미지 변경
+
+        return () => clearInterval(interval);
+    }, [images.length]);
+
+    return (
+        <div className="travel_wrap">
+            <div className="BG">
+                <div className="image-container">
+                    {images.map((image, index) => (
+                        <img
+                            key={index}
+                            src={image}
+                            alt={`Travel ${index + 1}`}
+                            className={index === currentImageIndex ? 'visible' : ''}
+                        />
+                    ))}
+                </div>
+                <div className="header_wrap">
+                    <button className='Home'>마크위치</button>
+                    <div className="login_but">
+                        <button className='Login'>로그인</button>
+                        <button className='Sing'>회원가입</button>
+                    </div>
+                </div>
+                <div className="bottom_wrap">
+                    <h3>테마 여행</h3>
+                    <div className="search_wrap">
+                        <form action="">
+                            <Input
+                                type="text"
+                                placeholder="원하시는 검색을 입력해주세요."
+                            />
+                        </form>
+                    </div>
+                    <h6>다양한 테마를 기반으로 한 여행에 대해 알아보고 <br />
+                    테마 구경부터 예약까지 한 번에.</h6>
+                </div>
+            </div>
+            <div className="Trv_main">
+                <div className="Trv_container">
+                    <div className="try_title">
+                        <h4>여행지 추천</h4>
+                        <div className="when">
+                            <div className="in">국내</div>
+                            <div className="out">해외</div>
+                        </div>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+        
+    );
+};
+
+export default Travel;

06d5139afb03654b681220d5b2ea758a54ca4a23
leeyouna21 <yuna5845@gmail.com>
2024-05-27 17:19:13 +0900

    twi

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Main from "./components/Main/Main";
+import MainPG from "./components/MainPG/MainPg";
 import Login from "./components/Login/Login";
 import Travel from "./components/Travel/Travel";
 import Domestic from "./components/Travel/Domestic";
@@ -14,8 +14,8 @@ function App() {
     <div className="App">
       <Router>
         <Routes>
-          <Route path="/" element={<Navigate to="/main" />} />
-          <Route path="/Main" element={<Main />} />
+          <Route path="/" element={<Navigate to="/MainPG" />} />
+          <Route path="/MainPG" element={<MainPG />} />
           <Route path="/Login" element={<Login />} />
           <Route path="/Travel" element={<Travel />} />
           <Route path="/TravelItem" element={<TravelItem />} />
---
+++ src/components/MainPG/MainPg.jsx
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from 'react';
+
+const MainPg = () => {
+  return (
+    <div>
+        <div className="main_wrap">
+            <div className="main_top_container">
+                <div className="main_search">
+                    <form action="">
+                        <input type="text" 
+                            placeholder='원하시는 검색어를 입력해주세요.'
+                        />
+                    </form>
+                </div>
+                <div className="category_wrap">
+                    <div className="icon">
+                        <img className='iconImg' src="/image/health.png" alt="" />
+                        <h4>건강</h4>
+                    </div>
+                    <div className="icon">
+                        <img className='iconImg' src="/image/travel.png" alt="" />
+                        <h4>여행</h4>
+                    </div>
+                    <div className="icon">
+                        <img className='iconImg' src="/image/hobby.png" alt="" />
+                        <h4>취미</h4>
+                    </div>
+                    <div className="icon">
+                        <img className='iconImg' src="/image/money.png" alt="" />
+                        <h4>일자리</h4>
+                    </div>
+                    <div className="icon">
+                        <img className='iconImg' src="/image/education.png" alt="" />
+                        <h4>교육</h4>
+                    </div>
+                    <div className="icon">
+                        <img className='iconImg' src="/image/book.png" alt="" />
+                        <h4>도서</h4>
+                    </div>
+                    <div className="icon">
+                        <img className='iconImg' src="/image/running.png" alt="" />
+                        <h4>운동</h4>
+                    </div>
+                </div>
+            </div>
+            <div className="main_btm_container"></div>
+        </div>
+    </div>
+  )
+}
+
+export default MainPg

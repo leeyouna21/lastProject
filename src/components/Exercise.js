81f2b766db22e308b331449d6908f7303b7e78a0
Moowonn <rlaandnsj11@naver.com>
2024-06-09 21:06:47 +0900

    영양제 사진 추가

--- src/components/Exercise.js
+++ src/components/Exercise.js
@@ -43,100 +43,72 @@ export const UpperData = [
     },
     {
         title: '푸쉬업',
-        location: '',
-        moreinfo: '',
-        imgSrc: '/image/health1.png',
+        location: '가슴, 어깨, 삼두근을 강화',
+        moreinfo: '여러 변형 동작으로 난이도 조절 가능',
+        imgSrc: '/image/health7.png',
         imgAlt: '운동'
     },
     {
-        title: '',
-        location: '',
-        moreinfo: '',
-        imgSrc: '/image/health1.png',
+        title: '오버헤드 프레스',
+        location: '어깨 근육, 삼두, 상체 코어',
+        moreinfo: '덤벨이나 바벨을 어깨 높이에서 머리 위로 드는 운동',
+        imgSrc: '/image/health8.png',
         imgAlt: '운동'
     },
     {
-        title: '',
-        location: '',
-        moreinfo: '',
-        imgSrc: '/image/health1.png',
+        title: '덤벨 로우',
+        location: '등, 이두근 강화',
+        moreinfo: '한 손으로 덤벨을 들어올리는 동작',
+        imgSrc: '/image/health9.png',
         imgAlt: '운동'
     },
     {
-        title: '',
-        location: '',
-        moreinfo: '',
-        imgSrc: '/image/health1.png',
+        title: '바벨 로우',
+        location: '등 전체와 이두근 타겟',
+        moreinfo: '바벨을 허리 쪽으로 당기는 동작',
+        imgSrc: '/image/health10.png',
         imgAlt: '운동'
     },
     {
-        title: '',
-        location: '',
-        moreinfo: '',
-        imgSrc: '/image/health1.png',
+        title: '덤벨 플라이',
+        location: '가슴 근육 타겟',
+        moreinfo: '덤벨을 사용해 팔을 벌렸다가 모으는 동작',
+        imgSrc: '/image/health11.png',
         imgAlt: '운동'
     },
     {
-        title: '',
-        location: '',
-        moreinfo: '',
-        imgSrc: '/image/health1.png',
+        title: '딥스',
+        location: '가슴, 삼두근 타겟',
+        moreinfo: '두 팔로 몸을 지탱하여 미는 동작',
+        imgSrc: '/image/health12.png',
         imgAlt: '운동'
     },
     {
-        title: '',
-        location: '',
-        moreinfo: '',
-        imgSrc: '/image/health1.png',
+        title: '레터럴 레이즈',
+        location: '어깨 측면 타겟',
+        moreinfo: '덤벨을 사용해 팔을 옆으로 들어올림',
+        imgSrc: '/image/health13.png',
         imgAlt: '운동'
     },
     {
-        title: '',
-        location: '',
-        moreinfo: '',
-        imgSrc: '/image/health1.png',
+        title: '프론트 레이즈',
+        location: '어깨 전면 타겟',
+        moreinfo: '덤벨을 사용해 팔을 앞쪽으로 들어올림',
+        imgSrc: '/image/health14.png',
         imgAlt: '운동'
     },
     {
-        title: '',
-        location: '',
-        moreinfo: '',
-        imgSrc: '/image/health1.png',
+        title: '케이블 크로스오버',
+        location: '가슴 근육을 타겟',
+        moreinfo: '케이블을 이용해 팔을 교차시키며 가슴을 수축',
+        imgSrc: '/image/health15.png',
         imgAlt: '운동'
     },
     {
-        title: '',
-        location: '',
-        moreinfo: '',
-        imgSrc: '/image/health1.png',
-        imgAlt: '운동'
-    },
-    {
-        title: '',
-        location: '',
-        moreinfo: '',
-        imgSrc: '/image/health1.png',
-        imgAlt: '운동'
-    },
-    {
-        title: '',
-        location: '',
-        moreinfo: '',
-        imgSrc: '/image/health1.png',
-        imgAlt: '운동'
-    },
-    {
-        title: '',
-        location: '',
-        moreinfo: '',
-        imgSrc: '/image/health1.png',
-        imgAlt: '운동'
-    },
-    {
-        title: '',
-        location: '',
-        moreinfo: '',
-        imgSrc: '/image/health1.png',
+        title: '페이스 풀',
+        location: '등 상부 및 어깨 후면 타겟',
+        moreinfo: '케이블 머신을 활용해 얼굴 쪽으로 당기는 동작',
+        imgSrc: '/image/health16.png',
         imgAlt: '운동'
     }
 ];
@@ -181,6 +153,48 @@ export const LowerData = [
         location: '허벅지 안쪽 근육',
         moreinfo: '내전근 기능 강화',
         imgSrc: '/image/lower6.png',
-        imgAlt: '도서'
-    }   
+        imgAlt: '운동'
+    },
+    {
+        title: '런지',
+        location: '대퇴사두근, 엉덩이 타겟',
+        moreinfo: '바벨 덤벨등을 들고 해당 동작 시행',
+        imgSrc: '/image/lower7.png',
+        imgAlt: '운동'
+    },
+    {
+        title: '카프레이즈',
+        location: '종아리 근육 타겟',
+        moreinfo: '서서 발뒤꿈치를 들어올리는 동작',
+        imgSrc: '/image/lower8.png',
+        imgAlt: '운동'
+    },
+    {
+        title: '스텝업',
+        location: '대퇴사두근, 햄스트링 타겟',
+        moreinfo: '박스나 벤치 위로 올라가는 동작',
+        imgSrc: '/image/lower9.png',
+        imgAlt: '운동'
+    },
+    {
+        title: '케틀벨 스윙',
+        location: '엉덩이, 햄스트링 타겟',
+        moreinfo: '케틀벨을 스윙하는 동작',
+        imgSrc: '/image/lower10.png',
+        imgAlt: '운동'
+    },
+    {
+        title: '덤벨 스쿼트',
+        location: '대퇴사두근, 햄스트링 타겟',
+        moreinfo: '덤벨을 양손에 들고 스쿼트를 수행',
+        imgSrc: '/image/lower11.png',
+        imgAlt: '운동'
+    },
+    {
+        title: '싱글 레그 데드리프트',
+        location: '햄스트링, 엉덩이 타겟',
+        moreinfo: '한 발로 서서 다른 발을 뒤로 들며 몸을 앞으로 굽힘',
+        imgSrc: '/image/lower12.png',
+        imgAlt: '운동'
+    }
 ];

f6f31983f4fbd8fc2e9754a4456f65404c12ba7e
Hidetoshi <hidetoshi0623@hotmail.com>
2024-06-10 00:01:31 +0900

    code

--- src/components/Hobby.js
+++ src/components/Hobby.js
@@ -21,62 +21,56 @@ export const RidingData = [
         thumbnail: '/image/classic.png'
     },
     {
-        title: '수공예 모임',
-        desc: '수공예 동호회',
-        hashes: ['#수공예', '#손작업'],
-        link: 'https://gall.dcinside.com/board/lists/?id=handicraft',
-        thumbnail: ''
-    },
-    {
         title: '요리 모임',
         desc: '요리 동호회',
         hashes: ['#요리', '#레시피'],
         link: 'http://www.lampcook.com/',
-        thumbnail: ''
+        thumbnail: '/image/Cooking.png'
     },
     {
         title: '퍼즐 모임',
         desc: '퍼즐 동호회',
         hashes: ['#퍼즐', '#두뇌게임'],
         link: 'https://divedice.net/board/game?ca=%ED%8D%BC%EC%A6%90',
-        thumbnail: ''
+        thumbnail: '/image/Puzzle.png'
     },
     {
         title: '영화 감상 모임',
         desc: '영화 감상 동호회',
         hashes: ['#영화', '#감상', '#명작'],
         link: 'https://extmovie.com/',
-        thumbnail: ''
+        thumbnail: '/image/Movie.png'
     },
     {
         title: '그림 그리기 모임',
         desc: '그림 그리기 동호회',
         hashes: ['#그림', '#미술'],
         link: 'https://grafolio.ogq.me/project/spotlight',
-        thumbnail: ''
+        thumbnail: '/image/Drawing.png'
     },
     {
         title: '바둑 모임',
         desc: '바둑 동호회',
         hashes: ['#바둑', '#전략'],
         link: 'https://www.tygem.com/main.php',
-        thumbnail: ''
+        thumbnail: '/image/Baduk.png'
     },
     {
         title: '장기 모임',
         desc: '장기 동호회',
         hashes: ['#장기', '#보드게임'],
         link: 'http://www.kja.or.kr/data_room/janggi_site.php',
-        thumbnail: ''
+        thumbnail: '/image/Janggi.png'
     },
     {
         title: '정원 모임',
         desc: '정원 동호회',
         hashes: ['#정원', '#식물'],
         link: 'https://garden.koagi.or.kr/cpage/garden/national.do',
-        thumbnail: ''
+        thumbnail: '/image/Gardening.png'
     }
 ];
+
 export const FishingData = [
     {
         title: '낚시 모임',
@@ -110,35 +104,35 @@ export const FishingData = [
         title: '탁구 모임',
         desc: '탁구 동호회',
         hashes: ['#탁구', '#탁구', '#탁구', '#탁구친구'],
-        link: 'https://m.trevari.co.kr/',
-        thumbnail: '/image/book.png'
+        link: 'https://m.cafe.daum.net/youngintt/_rec',
+        thumbnail: '/image/TableTennis.png'
     },
     {
         title: '요가 모임',
         desc: '요가 동호회',
         hashes: ['#요가', '#명상'],
         link: 'https://www.hohoyoga.com/',
-        thumbnail: ''
+        thumbnail: '/image/Yoga.png'
     },
     {
         title: '수영 모임',
         desc: '수영 동호회',
         hashes: ['#수영', '#물놀이'],
         link: 'https://suwimi.com/',
-        thumbnail: ''
+        thumbnail: '/image/Swimming.png'
     },
     {
         title: '자전거 타기 모임',
         desc: '자전거 타기 동호회',
         hashes: ['#자전거', '#라이딩'],
         link: 'https://bikesell.co.kr/site/BOARD/guide3.asp?doltop=MAIN',
-        thumbnail: ''
+        thumbnail: '/image/Biking.png'
     },
     {
         title: '테니스 모임',
         desc: '테니스 동호회',
         hashes: ['#테니스', '#운동', '#구기'],
         link: 'https://www.smaxh.com/',
-        thumbnail: ''
+        thumbnail: '/image/Tennis.png'
     }
 ];
